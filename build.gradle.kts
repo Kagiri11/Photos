c4fc42dcbee9b393ab9fa44ed9e149ca09dd9cfa
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-06-12 23:23:10 +0300

    added ktLint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-buildscript {
-    val composeVersion = "1.2.0-alpha01"
-} // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "7.1.3" apply false
     id("com.android.library") version "7.1.3" apply false
@@ -8,6 +5,40 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.5.30" apply false
 }
 
+val ktlint by configurations.creating
+
+val outputDir = "${project.buildDir}/reports/ktlint/"
+
+val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
+
+val ktlintCheck by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    args = listOf("src/**/*.kt")
+}
+
+val ktlintFormat by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    args = listOf("-F", "src/**/*.kt")
+}
+
 tasks.register(name = "type", type = Delete::class) {
     delete(rootProject.buildDir)
+}
+
+dependencies {
+    ktlint("com.pinterest:ktlint:0.45.2") {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
+    }
 }
