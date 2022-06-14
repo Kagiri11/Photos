5e56c534ae478770e086105dc817244eca85678f
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-06-14 21:21:07 +0300

    ktlintFormat

--- presentation/src/main/java/com/cmaina/presentation/screens/TestScreen.kt
+++ presentation/src/main/java/com/cmaina/presentation/screens/TestScreen.kt
@@ -1,18 +1,7 @@
 package com.cmaina.presentation.screens
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import com.cmaina.presentation.components.photostext.PhotosTitleText
-import com.cmaina.presentation.components.photostext.FotosText
-import com.cmaina.presentation.ui.theme.FotosBlack
 
 @Composable
 fun MainScreen() {
-
 }
