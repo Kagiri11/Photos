58f5e02f36949c43ca1e128384c985b50ddf4488
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-07-09 13:39:42 +0300

    removed header references

--- data/network/src/main/java/com/cmaina/network/api/PhotosRemoteSource.kt
+++ data/network/src/main/java/com/cmaina/network/api/PhotosRemoteSource.kt
@@ -6,7 +6,6 @@ import com.cmaina.network.models.search.PhotoSearchResultDto
 import com.cmaina.network.models.specificphoto.SpecificPhoto
 import com.skydoves.sandwich.ApiResponse
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -19,24 +18,19 @@ interface PhotosRemoteSource {
     @GET("photos/{id}")
     suspend fun fetchPhoto(
         @Path("id") id: String,
-        @Header("Authorization") authorization: String = "Client-ID pbq2xfRl6EbYjlRQeGfkp5dBfdzSuETZQiBPrbSSswk"
     ): ApiResponse<SpecificPhoto>
 
     @GET("photos/random")
-    suspend fun fetchRandomPhoto(
-        @Header("Authorization") authorization: String = "Client-ID pbq2xfRl6EbYjlRQeGfkp5dBfdzSuETZQiBPrbSSswk"
-    ): ApiResponse<PhotoListItem>
+    suspend fun fetchRandomPhoto(): ApiResponse<PhotoListItem>
 
     @GET("photos/{id}/like")
     suspend fun likePhoto(
         @Path("id") id: String,
-        @Header("Authorization") authorization: String = "Client-ID pbq2xfRl6EbYjlRQeGfkp5dBfdzSuETZQiBPrbSSswk"
     ): ApiResponse<PhotoListItem>
 
     @GET("photos/{id}/statistics")
     suspend fun fetchPhotoStatistics(
         @Path("id") id: String,
-        @Header("Authorization") authorization: String = "Client-ID pbq2xfRl6EbYjlRQeGfkp5dBfdzSuETZQiBPrbSSswk"
     ): ApiResponse<PhotoStatistics>
 
     @GET("search/photos")
