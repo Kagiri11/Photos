7ce56880d763418f94c4aac7d57da2cd92f97180
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-08-07 16:15:14 +0300

    Update Configurations.kt

--- buildSrc/src/main/java/Configurations.kt
+++ buildSrc/src/main/java/Configurations.kt
@@ -2,7 +2,7 @@ object Configurations {
     const val CompileSdk = 32
     const val MinSdk = 21
     const val TargetSdk = 32
-    const val VersionName = "0.1.5"
-    const val VersionCode = 13
+    const val VersionName = "0.1.6"
+    const val VersionCode = 14
     const val ApplicationId = "com.cmaina.fotos"
 }
