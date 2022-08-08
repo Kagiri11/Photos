6e8d0bfa023a443fff7b0d68dba72537838a3f6b
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-08-08 16:36:25 +0300

    bumped up version

--- buildSrc/src/main/java/Configurations.kt
+++ buildSrc/src/main/java/Configurations.kt
@@ -2,7 +2,7 @@ object Configurations {
     const val CompileSdk = 32
     const val MinSdk = 21
     const val TargetSdk = 32
-    const val VersionName = "0.1.6"
-    const val VersionCode = 14
+    const val VersionName = "0.1.10"
+    const val VersionCode = 18
     const val ApplicationId = "com.cmaina.fotos"
 }
