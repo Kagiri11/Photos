63e8fa3c1d49dfe48fdf39e8f3dd46752a39b301
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-09-11 22:10:49 +0300

    bumped up versions

--- buildSrc/src/main/java/Configurations.kt
+++ buildSrc/src/main/java/Configurations.kt
@@ -2,7 +2,7 @@ object Configurations {
     const val CompileSdk = 33
     const val MinSdk = 21
     const val TargetSdk = 32
-    const val VersionName = "0.1.14"
-    const val VersionCode = 22
+    const val VersionName = "0.1.15"
+    const val VersionCode = 23
     const val ApplicationId = "com.cmaina.fotos"
 }
