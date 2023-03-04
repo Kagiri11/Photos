e0f6283287aac85e20cfaabd7b94fe783dacb3e5
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-03-04 11:58:20 +0300

    Adds List screen and photo item

--- app/src/main/java/com/cmaina/tacc/screens/list/ListScreen.kt
+++ app/src/main/java/com/cmaina/tacc/screens/list/ListScreen.kt
@@ -1,2 +1,39 @@
 package com.cmaina.tacc.screens.list
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import org.koin.androidx.compose.getViewModel
+
+@Composable
+fun ListScreen(listViewModel: ListViewModel = getViewModel()) {
+    val photos = listViewModel.marsPhotos.collectAsState().value
+    Surface(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(photos) {
+                MarsPhotoItem(imageAndUrl = it.imgSrc to it.id)
+            }
+        }
+    }
+}
+
+@Composable
+fun MarsPhotoItem(imageAndUrl: Pair<String, Int>) {
+    val painter = rememberAsyncImagePainter(model = imageAndUrl.first)
+    Image(
+        painter = painter,
+        contentDescription = "A picture of mars",
+        modifier = Modifier.size(100.dp).clip(RoundedCornerShape(20.dp))
+    )
+}
--- app/src/main/java/com/cmaina/tacc/screens/list/ListViewModel.kt
+++ app/src/main/java/com/cmaina/tacc/screens/list/ListViewModel.kt
@@ -1,19 +1,19 @@
 package com.cmaina.tacc.screens.list
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cmaina.domain.PhotosRepository
 import com.cmaina.domain.models.DomainPhoto
 import com.cmaina.domain.utils.NetworkResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class ListViewModel(private val photosRepository: PhotosRepository) : ViewModel() {
 
-    private val _marsPhotos: MutableLiveData<List<DomainPhoto>> = MutableLiveData()
-    val marsPhotos: LiveData<List<DomainPhoto>> get() = _marsPhotos
+    private val _marsPhotos = MutableStateFlow<List<DomainPhoto>>(emptyList())
+    val marsPhotos: StateFlow<List<DomainPhoto>> get() = _marsPhotos
 
     fun fetchPhotos() {
         viewModelScope.launch {
