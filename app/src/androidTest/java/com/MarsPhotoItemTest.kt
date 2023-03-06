3c3628b8b5a4d54e839e3ff1411a939da86a0f01
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-03-06 11:13:15 +0300

    Updates tests

--- app/src/androidTest/java/com/MarsPhotoItemTest.kt
+++ app/src/androidTest/java/com/MarsPhotoItemTest.kt
@@ -1,10 +1,8 @@
 package com
 
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.cmaina.tacc.screens.list.MarsPhotoItem
@@ -22,31 +20,15 @@ class MarsPhotoItemTest {
 
     @Test
     fun marsPhotoItem_onClick() {
-        var clicked = false
-        val onClicked = { clicked = true }
-        val imageAndUrl = "https://some.url/to/mars/photo.jpg"
+        var hasBeenClicked = false
+        val imageAndUrl = ""
 
         composeTestRule.setContent {
-            MarsPhotoItem(imageAndUrl = imageAndUrl, onClicked = onClicked)
+            MarsPhotoItem(imageAndUrl = imageAndUrl, onClicked = { hasBeenClicked = true })
         }
 
-        // Click on the Image composable
         composeTestRule.onNode(hasClickAction()).performClick()
 
-        // Check that onClick is triggered
-        assertTrue(clicked)
-    }
-
-    @Test
-    fun marsPhotoItem_contentDescription() {
-        val imageAndUrl = "https://some.url/to/mars/photo.jpg"
-
-        composeTestRule.setContent {
-            MarsPhotoItem(imageAndUrl = imageAndUrl, onClicked = {})
-        }
-
-        // Check the content description of the Image composable
-        composeTestRule.onNodeWithContentDescription("A picture of mars")
-            .assertIsDisplayed()
+        assertTrue(hasBeenClicked)
     }
 }
