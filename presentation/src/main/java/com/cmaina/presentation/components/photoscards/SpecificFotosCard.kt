b0ccfafdc57657177eff9b7bff0c946a10ea6bd1
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-09-09 23:52:20 +0300

    chore: replaces accompanist pager with foundation pager

--- presentation/src/main/java/com/cmaina/presentation/components/photoscards/SpecificFotosCard.kt
+++ presentation/src/main/java/com/cmaina/presentation/components/photoscards/SpecificFotosCard.kt
@@ -1,9 +1,13 @@
 package com.cmaina.presentation.components.photoscards
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
@@ -11,24 +15,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.util.lerp
 import com.cmaina.presentation.screens.photodetails.PhotoLikedState
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.calculateCurrentOffsetForPage
 import kotlin.math.absoluteValue
 
-@OptIn(ExperimentalPagerApi::class)
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ColumnScope.PhotosPager(images: List<PhotoLikedState>, onPageSwapped: (String) -> Unit) {
+    val pagerState = rememberPagerState()
     HorizontalPager(
-        count = images.size,
-        modifier = Modifier.weight(0.7f).fillMaxWidth()
+        pageCount = images.size,
+        modifier = Modifier
+            .weight(0.7f)
+            .fillMaxWidth(),
+        state = pagerState
     ) { page ->
         onPageSwapped(images[page].photoId ?: "")
         Card(
             modifier = Modifier
                 .fillMaxHeight(0.95f)
-                .fillMaxWidth(0.95f)
-                .graphicsLayer {
+                .fillMaxWidth(0.95f),
+                /*.graphicsLayer {
                     val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
                     lerp(
                         start = 0.85f,
@@ -43,7 +49,7 @@ fun ColumnScope.PhotosPager(images: List<PhotoLikedState>, onPageSwapped: (Strin
                         stop = 1f,
                         fraction = 1f - pageOffset.coerceIn(0f, 1f)
                     )
-                },
+                },*/
             shape = RoundedCornerShape(2)
         ) {
             AsyncImageBlur(
