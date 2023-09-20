f1b489f1e902ba5567a312d9b4952d0e5e71994f
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-09-20 22:41:43 +0300

    Open up the network client to enable testability

--- data/network/src/main/java/com/cmaina/network/providers/EndpointsProvider.kt
+++ data/network/src/main/java/com/cmaina/network/providers/EndpointsProvider.kt
@@ -1,10 +1,12 @@
 package com.cmaina.network.providers
 
+import com.cmaina.network.Network
 import com.cmaina.network.api.AuthRemoteSource
-import com.cmaina.network.NetworkClient
 import com.cmaina.network.api.PhotosRemoteSource
 import com.cmaina.network.api.UsersRemoteSource
+import io.ktor.client.engine.cio.CIO
 
+val NetworkClient = Network(CIO.create()).httpClient
 val photosRemoteSource = PhotosRemoteSource(client = NetworkClient)
 val usersRemoteSource = UsersRemoteSource(client = NetworkClient)
 val authRemoteSource = AuthRemoteSource(client = NetworkClient)
