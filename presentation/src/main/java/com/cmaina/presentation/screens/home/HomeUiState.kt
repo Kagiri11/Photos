19edae256483ff075c213f46b89e676eb04d99b0
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-10-05 00:50:01 +0300

    Creates and passes down ui state

--- presentation/src/main/java/com/cmaina/presentation/screens/home/HomeUiState.kt
+++ presentation/src/main/java/com/cmaina/presentation/screens/home/HomeUiState.kt
@@ -2,10 +2,20 @@ package com.cmaina.presentation.screens.home
 
 import androidx.paging.PagingData
 import com.cmaina.domain.models.photos.DomainPhotoListItem
+import com.cmaina.presentation.screens.photodetails.Details
 import kotlinx.coroutines.flow.Flow
 
-data class HomeUiState(
+data class HomeState(
     val pagedPhotos: Flow<PagingData<DomainPhotoListItem>>? = null,
     val isLoading: Boolean,
     val errorMessage: String? = null
 )
+sealed interface HomeUiState{
+
+    data class Success(val pagedPhotos: Flow<PagingData<DomainPhotoListItem>>): HomeUiState
+
+    object Loading: HomeUiState
+
+    data class Error(val errorMessage: String): HomeUiState
+}
+
