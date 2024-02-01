33c5dbae5dfb58fdb128802e113aae53f6e30556
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2024-02-01 16:01:19 +0300

    Fixes result

--- shared/src/commonMain/kotlin/com/cmaina/fotos/shared/data/network/SafeApiCall.kt
+++ shared/src/commonMain/kotlin/com/cmaina/fotos/shared/data/network/SafeApiCall.kt
@@ -1,15 +1,17 @@
 package com.cmaina.fotos.shared.data.network
 
+import com.cmaina.fotos.shared.domain.utils.Result
+import com.cmaina.fotos.shared.domain.utils.Result.*
 import io.ktor.client.statement.HttpResponse
 
 
-suspend inline fun <T : Any, reified R : Any>safeApiCall(
+suspend inline fun <T : Any, reified R : Any> safeApiCall(
     apiCall: suspend () -> T
 ) =
     try {
-        Result.Success(data = apiCall.invoke())
+        Success(data = apiCall.invoke())
     } catch (e: Exception) {
-        Result.Error(errorDetails = e.message.toString())
+        Error(errorDetails = "")
     }
 
 class InOut<in T : Any, out R : Any>(val item: @UnsafeVariance T) {
@@ -20,8 +22,8 @@ class InOut<in T : Any, out R : Any>(val item: @UnsafeVariance T) {
     ): Result<@UnsafeVariance R> {
 
         return when (response.status.value) {
-            200 -> Result.Success(data = mapper.invoke(item))
-            else -> Result.Error(errorDetails = "")
+            200 -> Success(data = mapper.invoke(item))
+            else -> Error(errorDetails = "")
         }
     }
 }
