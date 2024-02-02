c799196cbd1cade4d67fc2a8ade5bb59f3025aaa
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2024-02-02 21:33:24 +0300

    uses the righ result class from utils

--- shared/src/commonMain/kotlin/com/cmaina/fotos/shared/data/repositories/AuthRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/cmaina/fotos/shared/data/repositories/AuthRepositoryImpl.kt
@@ -5,11 +5,13 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import com.cmaina.fotos.shared.data.Constants.UserAccessToken
+import com.cmaina.fotos.shared.data.mappers.toDomain
 import com.cmaina.fotos.shared.data.network.AuthRemoteSource
 import com.cmaina.fotos.shared.data.network.InOut
 import com.cmaina.fotos.shared.domain.models.auth.AuthDomainResponse
 import com.cmaina.fotos.shared.domain.repositories.AuthRepository
 import com.cmaina.fotos.shared.data.network.models.auth.AuthRemoteResponse
+import com.cmaina.fotos.shared.domain.utils.Result
 import io.ktor.client.call.body
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
--- shared/src/commonMain/kotlin/com/cmaina/fotos/shared/domain/repositories/AuthRepository.kt
+++ shared/src/commonMain/kotlin/com/cmaina/fotos/shared/domain/repositories/AuthRepository.kt
@@ -1,10 +1,11 @@
 package com.cmaina.fotos.shared.domain.repositories
 
 import com.cmaina.fotos.shared.domain.models.auth.AuthDomainResponse
+import com.cmaina.fotos.shared.domain.utils.Result
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
-    suspend fun authenticateUser(authCode: String): Result<com.cmaina.fotos.shared.domain.models.auth.AuthDomainResponse>
+    suspend fun authenticateUser(authCode: String): Result<AuthDomainResponse>
 
     suspend fun saveUserAuthentication(accessToken: String)
 
